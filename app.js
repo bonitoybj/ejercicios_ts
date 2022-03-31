80c7c7db17d45dae0c0aa216877e85c456c10847
bonitoybj <bonitoybj@gmail.com>
2022-03-31 13:34:42 -0300

    segundo

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+"use strict";
 //Ejercicios
 //1
 alert("Hello Word!!");
 //2
 document.write("Hello Word");
 //3
+document.write(3 + 5 + "\n \n");
+//4
+var nombre = prompt("insgrese su nombre");
+document.write("Hola " + nombre + "\n \n");
+//5
+var num1 = prompt("ingrese un numero");
+var num2 = prompt("ingrese otro numero");
+document.write("la suma es: " + (parseInt(num1) + parseInt(num2)) + "\n \n");
+//6
+var nu1 = prompt("ingrese un numero");
+var nu2 = prompt("ingrese otro numero");
+if (parseInt(nu1) > parseInt(nu2)) {
+    document.write(nu1);
+}
+else {
+    document.write(nu2);
+}
+
+//7
 
