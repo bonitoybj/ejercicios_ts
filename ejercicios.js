97f721051b0d1ae2ff9503efbad8e69424bfc8d1
bonitoybj <bonitoybj@gmail.com>
2022-04-07 13:12:40 -0300

    actualizacion ejercicios

--- ejercicios.js
+++ ejercicios.js
@@ -7,48 +7,58 @@ alert("Hello Word!!");
 document.write("Hello Word \n \n;");
 
 //3 Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
-document.write(3 + 5 + "\n \n;");
+document.getElementById("ej3").innerHTML = 3 + 5 ;
 
-//4Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga “Hola nombreUsuario”
+//4 Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga “Hola nombreUsuario”
 var nombre = prompt("insgrese su nombre");
-document.write("Hola " + nombre + "\n \n;");
+document.getElementById("ej4").innerHTML = "Hola " + nombre;
 
 //5 Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
 var num1 = prompt("ingrese un numero");
 var num2 = prompt("ingrese otro numero");
-document.write("la suma es: " + (parseInt(num1) + parseInt(num2)) + "\n \n;");
+var res = parseInt(num1) + parseInt(num2);
+document.getElementById("ej5").innerHTML = "la suma es: " + res;
 
 //6 Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
 var nu1 = prompt("ingrese un numero");
 var nu2 = prompt("ingrese otro numero");
 if (parseInt(nu1) > parseInt(nu2)) {
-    document.write(nu1 + "\n \n;");
+    document.getElementById("ej6").innerHTML = nu1;
 }
 else {
-    document.write(nu2 + "\n \n;");
+    document.getElementById("ej6").innerHTML = nu2 ;
 }
 
 //7 Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
-var n1 = prompt("ingrese un numero");
-var n2 = prompt("ingrese otro numero");
-var n3 = prompt("ingrese otro numero");
+var n1 = prompt("ej7: ingrese un numero");
+var n2 = prompt("ej7: ingrese otro numero");
+var n3 = prompt("ej7: ingrese otro numero");
 if(n1>n2 & n1>n3){
-    document.write(n1 + "\n \n;");
+    document.getElementById("ej7").innerHTML = n1;
 }
 else if (n2>n3){
-    document.write(n2 + "\n \n;");
+    document.getElementById("ej7").innerHTML = n2 ;
 }
 else{
-    document.write(n3 + "\n \n;");
+    document.getElementById("ej7").innerHTML = n3;
 }
 
 //8 Escribe un programa que pida un número y diga si es divisible por 2
 var n1 = prompt("ingrese un numero");
 if(n1%2 ===0){
-    document.write("es divisible por 2" + "\n \n;");
+    document.getElementById("ej8").innerHTML = n1 + " es divisible por 2";
 }
 else{
-    document.write("no es divisible por 2" + "\n \n;")
+    document.getElementById("ej8").innerHTML = n1 + " no es divisible por 2";
 }
 
 //9 Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
+var frase = prompt("ingrese una frase");
+var indice = [];
+for(var i=0; i<frase.length; i++){
+    if(frase[i].toLowerCase()==="a") indice.push(i);
+}
+document.getElementById("ej9").innerHTML = "la frase: " + frase + " tiene " + indice.length + " 'a'";
+
+//10 Escribe un programa que pida una frase y escriba las vocales que aparecen
+var frase1 = prompt("ingrese una frase");
